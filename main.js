15844c6231c532b33685a2c1ff84a4cb154ee11c
Wathanyu Suliamma <tan_sanook02@hotmail.com>
2022-07-16 21:20:47 +0700

    3 fix

--- main.js
+++ main.js
@@ -2,15 +2,19 @@ const app = Vue.createApp({
     data() {
         return {
             product: 'Shoes',
-            image: './assets/images/socks_green.jpg',
-            inStock: true,
+            brand: 'SE331',
+            
+            // image: './assets/images/socks_green.jpg',
+            // inStock: true,
             inventory: 100,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' }
+                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
+                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 }
             ],
-            cart: 0
+            cart: 0,
+            SelectedVariant: 0,
+            onSale:true
 
         }
     },
@@ -20,6 +24,27 @@ const app = Vue.createApp({
         },
         updateImage(variantImage) {
             this.image = variantImage
+        },
+        updateVariant(index) {
+            this.SelectedVariant = index;
         }
+    },
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product
+        },
+
+        image() {
+            return this.variants[this.SelectedVariant].image
+        },
+        inStock() {
+            return this.variants[this.SelectedVariant].quantity
+        },
+        onSales(){
+            return this.brand+'is onSale '+this.product
+        }
+
+
     }
+
 })
