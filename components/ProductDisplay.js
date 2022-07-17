754c9894b01c31b3fa9f9f43e6fbc5f37ad1f1d2
Wathanyu Suliamma <tan_sanook02@hotmail.com>
2022-07-17 22:28:06 +0700

    5.9-6.8

--- components/ProductDisplay.js
+++ components/ProductDisplay.js
@@ -1,81 +1,103 @@
 app.component ('product-display',{
-    props: {
-        premium: {
-            type: Boolean,
-            required: true
-        }
-    },
-    template:
-    /*html*/
-    `<div class="product-display">
-        <div class="product-container">
-            <div class="product-image">
-                <img v-bind:src="image">
-            </div>
-            <div class="product-info">
-                <h1>{{ title }} </h1>
-                <p v-if="inStock"> In Stock</p>
-                <p v-else> Out of Stock</p>
-                <p>Shipping: {{shipping}}</p>
-                <product-details :details="details"></product-details>
- <div
-                    v-for="variant, index) in variants"
-                    :key="variant.id"
-                    @mouseover="updateVariant(index)"
-                    class="color-circle"
-                    :style=" { backgroundColor: variant.color }">
-                </div>
-                <button
-                class="button"
-                :class="{disabledButton: !inStock}"
-                :disabled='!inStock'
-                v-on:click="addToCart">
-                Add to Cart
-                </button>
-            </div>
-        </div>
-    </div>`,
-    data() {
-        return {
-            product: 'Shoes',
-            brand: 'SE 331',
-            inventory: 100,
-            details: ['50% cotton', '30% wool', '20% polyester'],
-            variants: [
-                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
-                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 50 }
-            ],
-            activeClass: true,
-            selectedVariant:0,
-        }
-    },
-     methods: {
-        addToCart() {
-            this.cart += 1
-        },
-        updateImage(variantImage) {
-            this.image = variantImage
-        },
-        updateVariant(index) {
-            this.selectedVariant = index;
-        }
-    },
-    computed: {
-        title() {
-            return this.brand + ' ' + this.product
-        },
-        image() {
-            return this.variants[this.selectedVariant].image
-        },
-        inStock() {
-            return this.variants[this.selectedVariant].quantity
-        },
-        shipping() {
-            if (this.premium) {
-                return 'Free'
+        props: {
+            premium: {
+                type: Boolean,
+                required: true
             }
-            return 30
+        },
+    
+        template:
+          
+        /*html*/
+        `<div class="product-display">
+            <div class="product-container">
+                <div class="product-image">
+                    <img v-bind:src="image">
+                </div>
+                <div class="product-info">
+                    <h1>{{ title }} </h1>
+                    <p v-if="inStock"> In Stock</p>
+                    <p v-else> Out of Stock</p>
+                    <p>Shipping: {{shipping}}</p>
+                    <product-details :details="details"></product-details>
+                    <div
+                        v-for="variant, index) in variants"
+                        :key="variant.id"
+                        @mouseover="updateVariant(index)"
+                        class="color-circle"
+                        :style=" { backgroundColor: variant.color }">
+                    </div>
+                    <button
+                    class="button"
+                    :class="{disabledButton: !inStock}"
+                    :disabled='!inStock'
+                    v-on:click="addToCart">
+                    Add to Cart
+                    </button>
+                    <button
+                    class="button"
+                    v-on:click="removeFromCart">
+                    Remove
+                    </button>
+                </div>
+                <review-list  v-if="reviews.length" :reviews="reviews"></review-list>
+                <review-form @review-submited="addReview"></review-form>
+        
+        </div>`,
+        data() {
+            return {
+                product: 'Shoes',
+                brand: 'SE 331',
+                // image: './assets/images/socks_green.jpg',
+                // inStock: false,
+                inventory: 100,
+                details: ['50% cotton', '30% wool', '20% polyester'],
+                variants: [
+                   
+                    { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
+                      { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 50 }
+                ],
+                activeClass: true,
+                selectedVariant:0,
+                reviews:[]
+            }
+        },
+       
+         methods: {
+            addToCart() {
+                this.$emit ('add-to-cart', this.variants[this.selectedVariant].id)
+            },
+            updateImage(variantImage) {
+                this.image = variantImage
+            },
+            updateVariant(index) {
+               
+                this.selectedVariant = index;
+            },
+            
+        removeFromCart() {
+            this.$emit('remove-from-cart')
+        },
+        addReview(review) {
+            this.reviews.push(review)
         }
-    }
+        },
+        computed: {
    
-})
+            inStock() {
+                return this.variants[this.selectedVariant].quantity
+            },
+            onSale() {
+                return this.brand + ' ' + this.product + ' is on sale' 
+            },
+            shipping() {
+               
+                if (this.premium) {
+                    return 'Free'
+                }
+                return 30
+            }
+        }
+    })
+    
+   
--- main.js
+++ main.js
@@ -1,20 +1,23 @@
 const app = Vue.createApp({
     data() {
         return {
-            product: 'Shoes',
-            brand: 'SE331',
-            
-            // image: './assets/images/socks_green.jpg',
-            // inStock: true,
-            inventory: 100,
-            details: ['50% cotton', '30% wool', '20% polyester'],
-            variants: [
-                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
-                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 }
-            ],
-            cart: 0,
-            SelectedVariant: 0,
-            onSale:true
+            // product: 'Shoes',
+            // brand: 'SE 331',
+            // // image: './assets/images/socks_green.jpg',
+            // // inStock: false,
+            // inventory: 100,
+            // details: ['50% cotton', '30% wool', '20% polyester'],
+            // variants: [
+            //     // { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
+            //     // { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' }
+            //     { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
+            //     { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 }
+            // ],
+            // selectedVariant: 0,
+           
+            premium: true,
+           
+            cart: [0,0]
 
         }
     },
@@ -26,25 +29,32 @@ const app = Vue.createApp({
             this.image = variantImage
         },
         updateVariant(index) {
-            this.SelectedVariant = index;
+            this.selectedVariant = index
+        },
+        updateCart(id) {
+            if (id==2234) {
+                this.cart[0] += 1;
+            } else 
+                this.cart[1] += 1;
+        },
+        removeFromCart() {
+            this.cart.splice(0,1,0)
+            this.cart.splice(1,1,0)
         }
     },
     computed: {
         title() {
             return this.brand + ' ' + this.product
         },
-
-        image() {
-            return this.variants[this.SelectedVariant].image
+        image(){
+            return this.variants[this.selectedVariant].image
         },
         inStock() {
-            return this.variants[this.SelectedVariant].quantity
+            return this.variants[this.selectedVariant].quantity
         },
-        onSales(){
-            return this.brand+'is onSale '+this.product
+        onSale() {
+            return this.brand + ' ' + this.product + ' is on sale' 
         }
-
-
-    }
-
+    },
+    
 })
